2c5e4a37a75cd01455a2b93ca1d66f7363a70c8b
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-22 16:10:35 -0300

    Conteudo parte 3

---
+++ bloco_4/dia1/script_conteudo_parte3.js
@@ -0,0 +1,28 @@
+/* Primeira parte */
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientInfo.firstName);
+console.log(typeof patientInfo.lastName);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+ /* Segunda parte */
+ let base = 5;
+ let altura = 8;
+ let area = base * altura;
+ let perimetro = base*2 + altura *2;
+
+ console.log("\nValor da base: " + base);
+ console.log("Valor da altura: " + altura);
+ console.log("Valor da área: " + area);
+ console.log("Valor do perímetro: " + perimetro);
+
