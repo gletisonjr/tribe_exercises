2d04b5e9099668a62612911243d8f88aa980acc6
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-22 15:48:59 -0300

    Conteudo parte 2

---
+++ bloco_4/dia1/script.js
@@ -0,0 +1,8 @@
+const myName = 'Gletison Ramos da Silva Junior';
+const birthCity = 'Belo Horizonte';
+const birthYear = 2030;
+
+birthYear = 1992;
+
+console.log("Olá "+ myName + "!");
+console.log("Sim, você que nasceu em " + birthCity + " no ano de " + birthYear + ".");
