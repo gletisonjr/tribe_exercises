9bad0d9ec45c90f02d49c6dc0aaea9890367e586
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-22 18:19:30 -0300

    Exercicio 1 subtracao

--- bloco_4/dia1/exercicios/exercicio1_adicao.js
+++ bloco_4/dia1/exercicios/exercicio1_adicao.js
@@ -3,4 +3,4 @@ let b = Math.floor(Math.random() * 101);
 
 console.log("valor de A: " + a);
 console.log("valor de B: " + b);
-console.log("soma de A e B: " + (a+b));
+console.log("soma de A + B: " + (a+b));
---
+++ bloco_4/dia1/exercicios/exercicio1_subtracao.js
@@ -0,0 +1,6 @@
+let a = Math.floor(Math.random() * 101);
+let b = Math.floor(Math.random() * 101);
+
+console.log("valor de A: " + a);
+console.log("valor de B: " + b);
+console.log("subtração de A - B: " + (a-b));
