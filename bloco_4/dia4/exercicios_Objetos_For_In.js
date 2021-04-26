2dc9ab5a042dd01ba34e614153addee71c0b6ae4
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-26 19:15:21 -0300

    Exercicio Objetos e For/in parte 4

--- bloco_4/dia4/exercicios_Objetos_For_In.js
+++ bloco_4/dia4/exercicios_Objetos_For_In.js
@@ -6,13 +6,23 @@ let info = {
 
   // Exercicio 1
 console.log(`Bem-vinda, ${info.personagem}`);
+console.log();
 
 // Exercicio 2
 info['recorrente'] = 'Sim';
 console.log(info); 
+console.log();
 
 // Exercicio 3
 
 for (const informacao in info) {
     console.log(informacao);
 }
+console.log();
+
+// Exercicio 4
+
+for (const informacao in info) {
+    console.log(info[informacao]);
+}
+console.log();
