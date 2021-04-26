a7514fb9e292e14b6e48e6da16e7345ede11b05d
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-26 19:09:15 -0300

    Exercicio Objetos e For/in parte 2

--- bloco_4/dia4/exercicios_Objetos_For_In.js
+++ bloco_4/dia4/exercicios_Objetos_For_In.js
@@ -3,3 +3,10 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+
+  // Exercicio 1
+console.log(`Bem-vinda, ${info.personagem}`);
+
+// Exercicio 2
+info['recorrente'] = 'Sim';
+console.log(info); 
