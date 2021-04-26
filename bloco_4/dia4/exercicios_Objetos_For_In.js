9bd87db2b20f18cc26969614225a9d68cea217fd
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-04-26 19:24:15 -0300

    Exercicio Objetos e For/in parte 5

--- bloco_4/dia4/exercicios_Objetos_For_In.js
+++ bloco_4/dia4/exercicios_Objetos_For_In.js
@@ -26,3 +26,17 @@ for (const informacao in info) {
     console.log(info[informacao]);
 }
 console.log();
+
+// Exercício 5
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+console.log('Ambos recorrentes');
+
